d3863ae00505c02969d1b90cf1885b1c189231cf
inatomitk <35132611+inatomitk@users.noreply.github.com>
2020-05-13 18:24:03 +0900

    impl clock_time

--- lib/clock_time.rb
+++ lib/clock_time.rb
@@ -1,6 +1,83 @@
-require "clock_time/version"
+require 'active_support'
+require 'active_support/core_ext/numeric'
 
-module ClockTime
-  class Error < StandardError; end
-  # Your code goes here...
+class ClockTime
+  include Comparable
+
+  attr_reader :hour, :minute
+
+  class << self
+    def parse(string)
+      if string =~ /\A(\d{2})[^\d]?(\d{2})\z/
+        new($1.to_i, $2.to_i)
+      else
+        raise ArgumentError, 'invalid clock time'
+      end
+    end
+  end
+
+  def initialize(hour, min)
+    @hour = hour
+    @minute = min
+    if !valid_hour? || !valid_minute?
+      raise ArgumentError, 'invalid clock time'
+    end
+  end
+
+  def to_duration
+    @hour.hours + @minute.minutes
+  end
+
+  def to_time(date)
+    midnight = date.to_time
+    replace_time(midnight)
+  end
+
+  def next_time(base_time)
+    t = replace_time(base_time)
+    return t + 1.day if t < base_time
+    t
+  end
+
+  def prev_time(base_time)
+    t = replace_time(base_time)
+    return t - 1.day if t > base_time
+    t
+  end
+
+  def replace_time(time)
+    time.change(hour: @hour, min: @minute)
+  end
+
+  def <=>(clock_time)
+    to_duration <=> clock_time.to_duration
+  end
+
+  def +(augend)
+    if augend.is_a?(self.class)
+      augend = augend.to_duration
+    end
+    to_duration + augend
+  end
+
+  def -(minuend)
+    if minuend.is_a?(self.class)
+      minuend = minuend.to_duration
+    end
+    to_duration - minuend
+  end
+
+  def valid_hour?
+    return false unless @hour.is_a?(Integer)
+    return true if @hour >= 0 && @hour <= 24
+    false
+  end
+
+  def valid_minute?
+    return false unless @minute.is_a?(Integer)
+    return true if @minute >= 0 && @minute <= 60
+    false
+  end
 end
+
+require 'clock_time/span'
--- spec/clock_time_spec.rb
+++ spec/clock_time_spec.rb
@@ -1,9 +1,108 @@
+require 'clock_time'
+
 RSpec.describe ClockTime do
-  it "has a version number" do
-    expect(ClockTime::VERSION).not_to be nil
+  describe '#self.parse' do
+    it 'returns ClockTime instance' do
+      expect(ClockTime.parse('12:30')).to eq(ClockTime.new(12, 30))
+    end
+    it 'raises an exception when argument is invalid' do
+      expect { ClockTime.parse('12345') }.to raise_error(ArgumentError)
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  describe '#initialize' do
+    it 'raises an exception when hour is invalid' do
+      expect { ClockTime.new('a', 11) }.to raise_error(ArgumentError)
+    end
+    it 'raises an exception when minute is invalid' do
+      expect { ClockTime.new(5, '22') }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#to_duration' do
+    it 'returns ActiveSupport::Duration' do
+      c = ClockTime.new(10, 30)
+      expect(c.to_duration).to eq(10.hours + 30.minutes)
+    end
+  end
+
+  describe '#to_time' do
+    it 'calls replace_time' do
+      date = Date.parse('2020/05/05')
+      c = ClockTime.new(10, 30)
+      expect(c).to receive(:replace_time)
+        .with(Time.parse('2020/05/05 00:00'))
+
+      c.to_time(date)
+    end
+  end
+
+  describe '#next_time' do
+    it 'returns today time when replaced time is greater than base time' do
+      c = ClockTime.new(12, 0)
+      base_time = Time.parse('2020/05/05 10:00')
+      expect(c.next_time(base_time)).to eq(Time.parse('2020/05/05 12:00'))
+    end
+    it 'returns tomorrow time when replaced time is less than base time' do
+      c = ClockTime.new(12, 0)
+      base_time = Time.parse('2020/05/05 15:00')
+      expect(c.next_time(base_time)).to eq(Time.parse('2020/05/06 12:00'))
+    end
+  end
+
+  describe '#prev_time' do
+    it 'returns today time when replaced time is less than base time' do
+      c = ClockTime.new(12, 0)
+      base_time = Time.parse('2020/05/05 14:00')
+      expect(c.prev_time(base_time)).to eq(Time.parse('2020/05/05 12:00'))
+    end
+    it 'returns yesterday time when replaced time is greater than base time' do
+      c = ClockTime.new(12, 0)
+      base_time = Time.parse('2020/05/05 10:00')
+      expect(c.prev_time(base_time)).to eq(Time.parse('2020/05/04 12:00'))
+    end
+  end
+
+  describe '#+' do
+    it 'returns time substraction' do
+      c1 = ClockTime.new(10, 30)
+      c2 = ClockTime.new(5, 40)
+      expect(c1 + c2).to eq(16.hours + 10.minutes)
+    end
+  end
+
+  describe '#-' do
+    it 'returns time substraction' do
+      c1 = ClockTime.new(10, 30)
+      c2 = ClockTime.new(5, 40)
+      expect(c1 - c2).to eq(4.hours + 50.minutes)
+    end
+  end
+
+  describe '#<=>' do
+    describe '==' do
+      it 'returns true when hour and minute are equal' do
+        clock_time1 = ClockTime.new(10, 30)
+        clock_time2 = ClockTime.new(10, 30)
+        expect(clock_time1).to eq(clock_time2)
+      end
+      it 'returns false when hour is not equal' do
+        clock_time1 = ClockTime.new(10, 30)
+        clock_time2 = ClockTime.new(9, 30)
+        expect(clock_time1).not_to eq(clock_time2)
+      end
+      it 'returns false when minute is not equal' do
+        clock_time1 = ClockTime.new(10, 30)
+        clock_time2 = ClockTime.new(10, 29)
+        expect(clock_time1).not_to eq(clock_time2)
+      end
+    end
+    describe '<' do
+      it 'returns true when right value is big' do
+        clock_time1 = ClockTime.new(10, 30)
+        clock_time2 = ClockTime.new(10, 31)
+        expect(clock_time1).to be < clock_time2
+      end
+    end
   end
 end
